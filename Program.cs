7d7ac649d1682aee90ec01245f937f2cf601bce8
jacoposergio <djago32@gmail.com>
2022-11-02 17:09:19 +0100

    almost done

--- Program.cs
+++ Program.cs
@@ -17,26 +17,124 @@
 //BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a
 //8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
 
+
+Prodotto prodottoUtente = new Prodotto("televisore", 50);
+prodottoUtente.SetIva(20);
+//prodottoUtente.SetDescrizione("Samsung Series 4 UE24N4300AU UHD Smart TV61cm (24'') 1366x768px LED Wi-Fi");
+prodottoUtente.Stampa();
+
 public class Prodotto
 {
-    public int codice;
-    public string nome;
-    public string descrizione;
-    public int prezzo;
-    public int iva;
+    private int codice;
+    private string nome;
+    private string descrizione;
+    private double prezzo;
+    private double iva;
 
-    public Prodotto(string nome, int prezzo)
+    public Prodotto(string nome, double prezzo)
     {
         this.nome = nome;
         this.prezzo = prezzo;
+        descrizione = "nessuna descrizione disponibile";
+        codice = new Random().Next(1928, 8504);
     }
 
-    public int Codice()
+    public double PrezzoBase()
     {
-        codice = new Random().Next(1, 1001);
-        return codice;
+        return prezzo;
     }
 
+    public double PrezzoIva()
+    {
+        double prezzoConIva = prezzo + ((prezzo * iva)/ 100);
+        return prezzoConIva;
+    }
+
+    public string NomeEsteso()
+    {
+        return codice + nome;
+    }
+
+    public int GetCodice()
+    {
+        return this.codice;
+    }
+
+    public string GetNome()
+    {
+        return this.nome;
+    }
+
+    public void SetNome(string nome)
+    {
+      this.nome = nome;
+    }
+
+    public string Getdescrizione()
+    {
+        return this.descrizione;
+    }
+
+    public void SetDescrizione(string descrizione)
+    {
+        this.descrizione = descrizione;
+    }
+
+    public double GetPrezzo()
+    {
+        return this.prezzo;
+    }
+
+    public void SetPrezzo(double prezzo)
+    {
+        if(prezzo < 0)
+        {
+            this.prezzo = 0;
+        }
+        this.prezzo = prezzo;
+    }
+
+    public double GetIva()
+    {      
+        return this.iva;
+    }
+
+    public void SetIva(double iva)
+    {
+        if (iva <= 0)
+        {
+            this.iva = 1;
+        }
+        this.iva = iva;
+    }
+
+    public string PadLeft()
+    {
+        string codiceStringa = codice.ToString();
+        int[] arrayZeri = new int[8 - codiceStringa.Length];
+
+        for(int i = 0; i < arrayZeri.Length; i++)
+        {
+            arrayZeri[i] = 0;
+        }
+
+        string arrayZeriStringa = arrayZeri.ToString();
+        string codiceCompleto = arrayZeriStringa + codiceStringa;
+        return codiceCompleto;
+    }
+    public void Stampa()
+    {
+        Console.WriteLine("codice: " + codice);
+        Console.WriteLine("nome: " + nome);
+        Console.WriteLine("descrizione: " + descrizione);
+        Console.WriteLine("prezzo: " + prezzo + " euro");
+        Console.WriteLine("iva: " + iva + "%");
+        Console.WriteLine("prezzo con iva: " + PrezzoIva() + " euro");
+        Console.WriteLine("nome esteso: " + NomeEsteso() );
+        Console.WriteLine("il codice completo è: " + PadLeft());
+    }
 
 
 }
+
+
