d6423189a6f4a0ec31a037cdc2d7a6c0d393b562
jacoposergio <djago32@gmail.com>
2022-11-02 15:44:09 +0100

    inizio

--- Program.cs
+++ Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("ciao!");
+
+//Nel progetto csharp-oop-shop, creare la classe Prodotto che gestisce i prodotti dello shop. Un prodotto è caratterizzato da:
+//codice(numero intero)
+//nome
+//descrizione
+//prezzo
+//iva
+//Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter
+//e setter ed eventuali altri metodi di “utilità” per fare in modo che:
+//alla creazione di un nuovo prodotto il codice sia valorizzato con un numero random
+//Il codice prodotto sia accessibile solo in lettura
+//Gli altri attributi siano accessibili sia in lettura che in scrittura
+//Il prodotto esponga sia un metodo per avere il prezzo base che uno per avere il prezzo comprensivo di iva
+//Il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice + nome
+//Nella vostro programma principale, testate tutte le funzionalità della classe Prodotto.
+//BONUS: create un metodo che restituisca il codice con un pad left di 0 per arrivare a
+//8 caratteri (ad esempio codice 91 diventa 00000091, mentre codice 123445567 resta come è)
+
+public class Prodotto
+{
+    public int codice;
+    public string nome;
+    public string descrizione;
+    public int prezzo;
+    public int iva;
+
+    public Prodotto(string nome, int prezzo)
+    {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+
+    public int Codice()
+    {
+        codice = new Random().Next(1, 1001);
+        return codice;
+    }
+
+
+
+}
